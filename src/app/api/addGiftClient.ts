adedd88527a2dfd3dab0a99b29ae09b8424b672f
Oluwapelumi <fafyeepelumi04@gmail.com>
2025-07-08 10:16:31 -0700

    adding of gift working well

--- src/app/api/addGiftClient.ts
+++ src/app/api/addGiftClient.ts
@@ -1,42 +1,18 @@
-// lib/clientApi.ts or app/api/gift/client.ts
+// /lib/api/giftClient.ts
+// import { apiUploadFile } from "@/app/api/base";
 
-import axios from "axios";
-import { getToken } from "@/lib/getToken";
+import { apiUploadFile } from "@/lib/api";
 
-export const addGiftClient = async (
+export const addGifts = async (
   name: string,
-  value: string,
+  value: string | number,
   icon: File,
   token: string
-) => {
-
-  // const token = getToken();
-  const formData = new FormData();
-  formData.append("name", name);
-  formData.append("value", value);
-  formData.append("icon", icon);
-
-  // const response = await axios.post(
-  //   `${process.env.NEXT_PUBLIC_API_URL}/gifting/add-gift`,
-  //   formData,
-  //   {
-  //     headers: {
-  //       Authorization: `Bearer ${token}`,
-  //     },
-  //   }
-  // );
-
-  const response = await fetch(`${process.env.NEXT_PUBLIC_CLIENT_API_URL}/gifting/add`, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-    body: formData,
-  });
-
-  return await response.json();
-
-  
+): Promise<ApiResponse<IGifting>> => {
+  return await apiUploadFile<IGifting>(
+    "gifting/add",
+    icon,
+    token,
+    { name, value }
+  );
 };
-
-console.log("Requesting:", `${process.env.NEXT_PUBLIC_CLIENT_API_URL}/gifting/add`);
